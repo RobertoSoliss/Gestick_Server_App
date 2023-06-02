905b6a28739c11fda4a62e469d5a0bb565e5a7fd
RobertoSoliss <championssolis@gmail.com>
2023-06-02 13:30:31 -0600

    subida EXBT

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -2,7 +2,7 @@ import connect from "../database.js";
 
 const getAdmin = async (req, res) => {
     const connection = await connect();
-    const [rows] = await connection.query(`SELECT * FROM admin where idAdmin = ${req.params.idAdmin}`);
+    const [rows] = await connection.query(`SELECT * FROM ADMIN where idAdmin = ${req.params.idAdmin}`);
     //console.log(rows);
     res.json(rows);
 
@@ -11,7 +11,7 @@ const getAdmin = async (req, res) => {
 }
 const getidAdmin = async (req, res) => {
     const connection = await connect();
-    const [rows] = await connection.query(`SELECT idAdmin FROM admin;`);
+    const [rows] = await connection.query(`SELECT idAdmin FROM ADMIN;`);
     //console.log(rows);
     res.json(rows);
 }
@@ -19,7 +19,7 @@ const getidAdmin = async (req, res) => {
 const getEmpUrl = async (req, res) => {
     //console.log(`El id dentro del back es: ${req.params.idEmpleado}`)
     const connection = await connect();
-    const [rows] = await connection.query(`SELECT EmUrlimg from empleado  where idEmpleado = ${req.params.idEmpleado}`);
+    const [rows] = await connection.query(`SELECT EmUrlimg from EMPLEADO  where idEmpleado = ${req.params.idEmpleado}`);
     //console.log(rows);
     res.json(rows);
 }
@@ -27,7 +27,7 @@ const getEmpUrl = async (req, res) => {
 const getProductUrl = async (req, res) => {
     //console.log(`El id dentro del back es: ${req.params.idProductos}`)
     const connection = await connect();
-    const [rows] = await connection.query(`SELECT PrURLimg from productos WHERE idProductos = ${req.params.idProductos}`);
+    const [rows] = await connection.query(`SELECT PrURLimg from PRODUCTOS WHERE idProductos = ${req.params.idProductos}`);
     //console.log(rows);
     res.json(rows);
 }
@@ -35,7 +35,7 @@ const getProductUrl = async (req, res) => {
 const saveAdmin = async (req, res) => {
     const connection = await connect();
     //console.log(`LAS RESPUESTAS DEL BACK EN saveAdmin SON: ${req.body.idEmpleado}, ${req.body.EmNombre},  ${req.body.EmApat},  ${req.body.EmAmat}, ${req.body.EmContrasenna}, ${req.body.Admin_idAdmin}, ${req.body.Eimagen_idEimagen}`)
-    const [results] = await connection.query("insert into admin (idAdmin,AdNombre,AdAppat,AdApMat,AdEmail,AdContrasenna,Gestick_idGestick,Aactivo) values (?,?,?,?,?,?,?,?)", [req.body.idAdmin, req.body.AdNombre, req.body.AdAppat, req.body.AdApmat, req.body.AdEmail, req.body.AdContrasenna, 1, 1]);
+    const [results] = await connection.query("insert into ADMIN (idAdmin,AdNombre,AdAppat,AdApMat,AdEmail,AdContrasenna,Gestick_idGestick,Aactivo) values (?,?,?,?,?,?,?,?)", [req.body.idAdmin, req.body.AdNombre, req.body.AdAppat, req.body.AdApmat, req.body.AdEmail, req.body.AdContrasenna, 1, 1]);
 
     res.json({
         id: results.insertId,
@@ -46,20 +46,20 @@ const saveAdmin = async (req, res) => {
 
 const getEmpleadoId = async (req, res) => {
     const connection = await connect();
-    const [rows] = await connection.query(`SELECT idEmpleado FROM empleado;`);
+    const [rows] = await connection.query(`SELECT idEmpleado FROM EMPLEADO;`);
     //console.log(rows);
     res.json(rows);
 }
 
 const getProductos = async (req, res) => {
     const connection = await connect();
-    const [rows] = await connection.query(`SELECT * FROM productos where Admin_idAdmin = ${req.params.idAdmin}`);
+    const [rows] = await connection.query(`SELECT * FROM PRODUCTOS where Admin_idAdmin = ${req.params.idAdmin}`);
     res.json(rows);
 }
 
 const getProductoId = async (req, res) => {
     const connection = await connect();
-    const [rows] = await connection.query(`SELECT idProductos from productos`);
+    const [rows] = await connection.query(`SELECT idProductos from PRODUCTOS`);
     //console.log(rows);
     res.json(rows);
 }
@@ -67,7 +67,7 @@ const getProductoId = async (req, res) => {
 const saveProducto = async (req, res) => {
     const connection = await connect();
     //console.log(`LAS RESPUESTAS DEL BACK EN SAVEPRODUCTO SON: ${req.body.idEmpleado}, ${req.body.EmNombre},  ${req.body.EmApat},  ${req.body.EmAmat}, ${req.body.EmContrasenna}, ${req.body.Admin_idAdmin}, ${req.body.Eimagen_idEimagen}`)
-    const [results] = await connection.query("INSERT INTO productos(idProductos, PrNombre, PrPrecio, PrExistencias, PrDescripcion, Admin_idAdmin, Marca_idMarca, Categoria_idCategoria, Pcodigo, PrURLimg) VALUES (?,?,?,?,?,?,?,?,?,?)", [req.body.idProductos, req.body.PrNombre, req.body.PrPrecio, req.body.PrExistencias, req.body.PrDescripcion, req.body.Admin_idAdmin, req.body.Marca_idMarca, req.body.Categoria_idCategoria, 1, req.body.PrURLimg]);
+    const [results] = await connection.query("INSERT INTO PRODUCTOS(idProductos, PrNombre, PrPrecio, PrExistencias, PrDescripcion, Admin_idAdmin, Marca_idMarca, Categoria_idCategoria, Pcodigo, PrURLimg) VALUES (?,?,?,?,?,?,?,?,?,?)", [req.body.idProductos, req.body.PrNombre, req.body.PrPrecio, req.body.PrExistencias, req.body.PrDescripcion, req.body.Admin_idAdmin, req.body.Marca_idMarca, req.body.Categoria_idCategoria, 1, req.body.PrURLimg]);
 
     res.json({
         id: results.insertId,
@@ -77,7 +77,7 @@ const saveProducto = async (req, res) => {
 
 const getProducto = async (req, res) => {
     const connection = await connect();
-    const [rows] = await connection.query(`SELECT * FROM productos where idProductos = ${req.params.idProducto}`);
+    const [rows] = await connection.query(`SELECT * FROM PRODUCTOS where idProductos = ${req.params.idProducto}`);
     res.json(rows[0]);
 }
 
@@ -89,7 +89,7 @@ const getMarcas = async (req, res) => {
      */
     try {
         const connection = await connect();
-        const [rows] = await connection.query(`SELECT MarNombre FROM marca`);
+        const [rows] = await connection.query(`SELECT MarNombre FROM MARCA`);
         res.json(rows);
     } catch (error) {
         console.error('Error al obtener las marcas en getMarcas', error);
@@ -100,7 +100,7 @@ const getMarcas = async (req, res) => {
 const getCategorias = async (req, res) => {
     try {
         const connection = await connect();
-        const [rows] = await connection.query(`SELECT CatNombre FROM categoria`);
+        const [rows] = await connection.query(`SELECT CatNombre FROM CATEGORIA`);
         res.json(rows);
     } catch (error) {
         console.error('Error al obtener las marcas en getMarcas', error);
@@ -110,12 +110,12 @@ const getCategorias = async (req, res) => {
 
 const getTasks = async (req, res) => {
     const connection = await connect();
-    const [rows] = await connection.query(`SELECT * FROM empleado where Admin_idAdmin= ${req.params.idAdmin}`);
+    const [rows] = await connection.query(`SELECT * FROM EMPLEADO where Admin_idAdmin= ${req.params.idAdmin}`);
     res.json(rows);
 }
 const getTask = async (req, res) => {
     const connection = await connect();
-    const [rows] = await connection.query(`SELECT * FROM empleado where idEmpleado = ${req.params.idEmpleado}`);
+    const [rows] = await connection.query(`SELECT * FROM EMPLEADO where idEmpleado = ${req.params.idEmpleado}`);
     //console.log(rows);
     res.json(rows[0]);
 
@@ -124,7 +124,7 @@ const getTask = async (req, res) => {
 }
 const getTaskCount = async (req, res) => {
     const connection = await connect();
-    const [rows] = await connection.query(`SELECT COUNT(*) FROM empleado`);
+    const [rows] = await connection.query(`SELECT COUNT(*) FROM EMPLEADO`);
     res.json(rows[0]["COUNT(*)"]);
     /*    res.send("hello world from getTaskCount")
         console.log("hello world from getTaskCount");*/
@@ -133,7 +133,7 @@ const saveTask = async (req, res) => {
     const connection = await connect();
     //console.log(`LAS RESPUESTAS DEL BACK EN SAVE SON: ${req.body.idEmpleado}, ${req.body.EmNombre},  ${req.body.EmApat},  ${req.body.EmAmat}, ${req.body.EmContrasenna}, ${req.body.Admin_idAdmin}, ${req.body.Eimagen_idEimagen}`)
     //console.log(`EL VALOR DEL idEmpleado dentro del back node es : ${req.body.idEmpleado}`)
-    const [results] = await connection.query("INSERT INTO empleado(idEmpleado, EmNombre, EmApat, EmAmat, EmContrasenna, EmDireccion1,EmDireccion2,Admin_idAdmin,Admin_Gestick_idGestick,EmURLimg) VALUES (?,?,?,?,?,?,?,?,?,?)", [req.body.idEmpleado, req.body.EmNombre, req.body.EmApat, req.body.EmAmat, req.body.EmContrasenna, 1, 1, req.body.Admin_idAdmin, 1, req.body.EmURLimg]);
+    const [results] = await connection.query("INSERT INTO EMPLEADO(idEmpleado, EmNombre, EmApat, EmAmat, EmContrasenna, EmDireccion1,EmDireccion2,Admin_idAdmin,Admin_Gestick_idGestick,EmURLimg) VALUES (?,?,?,?,?,?,?,?,?,?)", [req.body.idEmpleado, req.body.EmNombre, req.body.EmApat, req.body.EmAmat, req.body.EmContrasenna, 1, 1, req.body.Admin_idAdmin, 1, req.body.EmURLimg]);
 
     res.json({
         id: results.insertId,
@@ -145,7 +145,7 @@ const saveTask = async (req, res) => {
 }
 const deleteTasks = async (req, res) => {
     const connection = await connect();
-    const result = await connection.query(`DELETE FROM empleado where idEmpleado = ${req.params.idEmpleado}`);
+    const result = await connection.query(`DELETE FROM EMPLEADO where idEmpleado = ${req.params.idEmpleado}`);
     /**    console.log(`EL RESULTADO DEL ID OBTENIDO EN DELETE ES: ${req.params.idEmpleado}`); */
     res.sendStatus(204);
 
@@ -155,7 +155,7 @@ const deleteTasks = async (req, res) => {
 
 const deleteProductos = async (req, res) => {
     const connection = await connect();
-    const result = await connection.query(`DELETE FROM productos WHERE idProductos = ${req.params.idProductos}`);
+    const result = await connection.query(`DELETE FROM PRODUCTOS WHERE idProductos = ${req.params.idProductos}`);
     /**    console.log(`EL RESULTADO DEL ID OBTENIDO EN DELETE ES: ${req.params.idEmpleado}`); */
     res.sendStatus(204);
 
@@ -165,7 +165,7 @@ const deleteProductos = async (req, res) => {
 
 const updateTasks = async (req, res) => {
     const connection = await connect()
-    const results = await connection.query(`UPDATE empleado SET EmNombre='${req.body.EmNombre}', EmApat='${req.body.EmApat}', EmAmat='${req.body.EmAmat}',EmContrasenna='${req.body.EmContrasenna}',EmURLimg='${req.body.EmURLimg}' WHERE idEmpleado=${req.body.idEmpleado}`)
+    const results = await connection.query(`UPDATE EMPLEADO SET EmNombre='${req.body.EmNombre}', EmApat='${req.body.EmApat}', EmAmat='${req.body.EmAmat}',EmContrasenna='${req.body.EmContrasenna}',EmURLimg='${req.body.EmURLimg}' WHERE idEmpleado=${req.body.idEmpleado}`)
     //console.log(results)
     res.sendStatus(204);
 
@@ -176,7 +176,7 @@ const updateTasks = async (req, res) => {
 
 const updateProducto = async (req, res) => {
     const connection = await connect()
-    const results = await connection.query(`UPDATE productos SET PrNombre = '${req.body.PrNombre}', PrExistencias = ${req.body.PrExistencias}, PrPrecio = ${req.body.PrPrecio}, PrDescripcion = '${req.body.PrDescripcion}', Marca_idMarca = ${req.body.Marca_idMarca}, Categoria_idCategoria = ${req.body.Categoria_idCategoria}, PrURLimg = '${req.body.PrURLimg}' WHERE idProductos = ${req.body.idProductos}`)
+    const results = await connection.query(`UPDATE PRODUCTOS SET PrNombre = '${req.body.PrNombre}', PrExistencias = ${req.body.PrExistencias}, PrPrecio = ${req.body.PrPrecio}, PrDescripcion = '${req.body.PrDescripcion}', Marca_idMarca = ${req.body.Marca_idMarca}, Categoria_idCategoria = ${req.body.Categoria_idCategoria}, PrURLimg = '${req.body.PrURLimg}' WHERE idProductos = ${req.body.idProductos}`)
     //console.log(results)
     res.sendStatus(204);
 }
