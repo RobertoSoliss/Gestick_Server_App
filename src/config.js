e09e523b0bce179d2552d9f9bb178cc20b2f68d1
RobertoSoliss <championssolis@gmail.com>
2023-06-02 13:28:05 -0600

    subida EXBT

--- src/config.js
+++ src/config.js
@@ -1,20 +1,21 @@
-import {config as dotenv} from 'dotenv';
+import { config as dotenv } from 'dotenv';
 dotenv();
 console.log(process.env.NICKNAME)
 
-export const dbdates={
+export const dbdates = {
+  user: process.env.USER || 'root',
+  password: process.env.PASSWORD || 'celisSOLIS1027',
+  host: process.env.HOST || 'localhost',
+  port: process.env.DBPORT || 3306,
+  database: process.env.DBNAME || 'mydb'
+
   /**
-   *   user:process.env.USER || 'root',
-    password:process.env.PASSWORD || 'celisSOLIS1027',
-    host:process.env.HOST || 'localhost',
-    port:process.env.DBPORT || 3306,
-    database:process.env.DBNAME || 'mydb'
+   * user:process.env.USER || 'root',
+  password:process.env.PASSWORD || 'rfbhAEG03UOygPEGBteZ',
+  host:process.env.HOST || 'containers-us-west-197.railway.app',
+  port:process.env.DBPORT || 7632,
+  database:process.env.DBNAME || 'railway'
    */
-    user:process.env.USER || 'root',
-    password:process.env.PASSWORD || 'rfbhAEG03UOygPEGBteZ',
-    host:process.env.HOST || 'containers-us-west-197.railway.app',
-    port:process.env.DBPORT || 7632,
-    database:process.env.DBNAME || 'railway'
 }
 
 /*export const PORT = process.env.PORT || 3000
